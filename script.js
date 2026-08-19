867268c7f5ffc90ebe34f56ccce7516a985426a9
Saffwan <work.mohammedsaffwam1@gmail.com>
2026-08-19 18:36:23 +0530

    Initial website

---
+++ script.js
@@ -0,0 +1,8 @@
+// MS Auto World
+// Basic website functionality
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("MS Auto World website loaded successfully.");
+
+});
